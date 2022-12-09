1abcd062b997e4db58e76c78b81163c7c336dac7
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2022-12-09 15:50:47 +0700

    update code css news

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -33,7 +33,7 @@ const News = () => {
                     />
                     <div className={cx("news-info")}>
                         <h2>
-                            Đi chợ online: Xu hướng lên ngôi mùa dịch Covid-19
+                            Cách chọn rau củ quả tươi ngon, không "ngậm độc"
                         </h2>
                         <div className={cx("news-info-user")}>
                             <h3>Tác giả: Nguyễn Anh Dũng | </h3>
@@ -47,9 +47,7 @@ const News = () => {
                         alt=""
                     />
                     <div className={cx("news-info")}>
-                        <h2>
-                            Đi chợ online: Xu hướng lên ngôi mùa dịch Covid-19
-                        </h2>
+                        <h2>Các loại ngũ cốc tốt cho sức khỏe</h2>
                         <div className={cx("news-info-user")}>
                             <h3>Tác giả: Nguyễn Anh Dũng | </h3>
                             <span> 10/07/2021</span>
@@ -62,9 +60,7 @@ const News = () => {
                         alt=""
                     />
                     <div className={cx("news-info")}>
-                        <h2>
-                            Đi chợ online: Xu hướng lên ngôi mùa dịch Covid-19
-                        </h2>
+                        <h2>Các cách chế biến món ăn từ rau xanh</h2>
                         <div className={cx("news-info-user")}>
                             <h3>Tác giả: Nguyễn Anh Dũng | </h3>
                             <span> 10/07/2021</span>
